d00a53a7f75ab186ccfe3d0a0929db986a7b337b
PilotDeveloperAccount <1038883524@qq.com>
2018-11-15 16:14:03 +0800

    优化

--- app/src/main/java/com/plgf/qqdemo/MainActivity.java
+++ app/src/main/java/com/plgf/qqdemo/MainActivity.java
@@ -3,18 +3,26 @@ package com.plgf.qqdemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-
     private QQinfo viewById;
-    private PayPsdInputView viewById1;
-
+    private PwdEditText viewById1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        viewById1 = (PayPsdInputView) findViewById(R.id.paypadinputview);
+        viewById1 = (PwdEditText) findViewById(R.id.paypadinputview);
+
+        viewById1.setOnTextChangeListener(new PwdEditText.OnTextChangeListener() {
+            @Override
+            public void onTextChange(String pwd) {
+                if (pwd.length() == viewById1.getTextLength()){
+                    //输入监听
+                    Toast.makeText(MainActivity.this,pwd, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
 
     }
