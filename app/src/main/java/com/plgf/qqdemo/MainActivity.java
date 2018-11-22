e9e9459b71827521f48f54b34dcf24d4b95b4d29
PilotDeveloperAccount <1038883524@qq.com>
2018-11-22 09:25:59 +0800

    优化

--- app/src/main/java/com/plgf/qqdemo/MainActivity.java
+++ app/src/main/java/com/plgf/qqdemo/MainActivity.java
@@ -3,11 +3,21 @@ package com.plgf.qqdemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
     private QQinfo viewById;
     private PwdEditText viewById1;
+    private TextView viewById2;
+    private EasyPickerView epvH;
+    private int hour;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +33,15 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        viewById2 = (TextView) findViewById(R.id.btn);
+        viewById2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "2222", Toast.LENGTH_SHORT).show();
+            }
+        });
 
+        initHours();
 
     }
 
@@ -39,5 +57,28 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onTouchEvent(event);
 
+    }
+
+    private void initHours() {
+        epvH = (EasyPickerView) findViewById(R.id.epv_h);
+        final ArrayList<String> hDataList = new ArrayList<>();
+        for (int i = 0; i < 24; i++)
+            hDataList.add("" + i);
+
+        epvH.setDataList(hDataList);
+        epvH.setOnScrollChangedListener(new EasyPickerView.OnScrollChangedListener() {
+            @Override
+            public void onScrollChanged(int curIndex) {
+                hour = Integer.parseInt(hDataList.get(curIndex));
+                //tv.setText(hour + "h" + minute + "m");
+            }
+
+            @Override
+            public void onScrollFinished(int curIndex) {
+                hour = Integer.parseInt(hDataList.get(curIndex));
+                //tv.setText(hour + "h" + minute + "m");
+                Toast.makeText(MainActivity.this, ""+hour, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
